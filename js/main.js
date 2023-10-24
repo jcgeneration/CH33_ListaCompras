21f28c5ee1cc6c2b8e9152edb721f2f2caf4f0a7
JC <juan.valencia@idr.edu.mx>
2023-10-24 12:10:23 -0600

    Se agregan los elementos a la tabla

--- js/main.js
+++ js/main.js
@@ -2,23 +2,74 @@
 let txtNombre = document.getElementById("Name"); // Nombre
 let txtNumber = document.getElementById("Number"); //Cantidad
 let btnAgregar = document.getElementById("btnAgregar");
+let btnClear = document.getElementById("btnClear");
 let alertValidaciones = document.getElementById("alertValidaciones");
+let tablaListaCompras = document.getElementById("tablaListaCompras");
+let cuerpoTabla = tablaListaCompras.getElementsByTagName("tbody").item(0);
+let contador = 0;
+let precio = 0;
 
 function validarCantidad(){
     if (txtNumber.value.length==0){
         return false;
     }// if length
+    if (isNaN(txtNumber.value)){
+        return false;
+    }//isNaN
+    if(parseFloat(txtNumber.value)<=0){
+        return false;
+    }//<=0
+    if(parseInt(txtNumber.value)!=txtNumber.value){
+        return false;
+    }//<=0
     return true;
 }//validarCantidad
 
+function getPrecio(){
+    return Math.floor(((Math.random() *100)*100))/100;
+}//getPrecio
+
 btnAgregar.addEventListener("click", function(event){
+    let isValid = true;
     event.preventDefault();
+    alertValidaciones.innerHTML="";
+    alertValidaciones.style.display="none";
+    txtNombre.style.border="";
+    txtNumber.style.border="";
     if (txtNombre.value.length<3) {
         alertValidaciones.innerHTML="El campo <strong>Nombre</strong> es requerido<br/>";
         alertValidaciones.style.display="block";
+        txtNombre.style.border="solid thin red";
+        isValid = false;
     }// txtNombre < 3
     if (! validarCantidad()){
         alertValidaciones.innerHTML+="El campo <strong>Cantidad</strong> es requerido<br/>";
         alertValidaciones.style.display="block";
+        txtNumber.style.border="solid thin red";
+        isValid = false;
     }//if ! validarCantidad
-});
+    if (isValid){
+        contador++;
+        precio = getPrecio();
+        let row = `<tr>
+                        <td>${contador}</td>
+                        <td>${txtNombre.value}</td>
+                        <td>${txtNumber.value}</td>
+                        <td>${precio}</td>
+                    </tr>`;
+        cuerpoTabla.insertAdjacentHTML("beforeend", row);
+        txtNombre.value="";
+        txtNumber.value="";
+    }//isValid
+});// btnAgregar click
+
+btnClear.addEventListener("click", function(event){
+    event.preventDefault();
+    alertValidaciones.innerHTML="";
+    alertValidaciones.style.display="none";
+    txtNombre.style.border="";
+    txtNumber.style.border="";
+    txtNombre.value="";
+    txtNumber.value="";
+});// btnClear click
+
